50f8c84b9f32c421783b253b763bcaf07607c08d
forestSpringH <huanglinchun@runjingchina.com>
2023-08-17 19:39:16 +0800

    init

---
+++ src/main/java/com/runjing/resolve_excel_auto/ResolveExcelAutoApplication.java
@@ -0,0 +1,13 @@
+package com.runjing.resolve_excel_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveExcelAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolveExcelAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/runjing/resolve_excel_auto/ResolveExcelAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runjing.resolve_excel_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveExcelAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
